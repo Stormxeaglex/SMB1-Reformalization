afad955d7f9b02b4fa75e2ab580ffa2f45756b27
Stormxeaglex <liamthoreson@gmail.com>
2026-02-22 01:52:13 -0600

    saving progress for the nigght. Might use external C files to make things easier

---
+++ PrintVictoryMessages.c
@@ -0,0 +1,8 @@
+void PrintVictoryMessages() { //Probably should change this to a label
+  if (SecondaryMsgCounter == 0) {
+    if (PrimaryMsgCounter != 0) { 
+    }
+    { //Thank Player?
+      
+    }
+  }
