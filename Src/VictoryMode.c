2f008a5939df1f42757e0f5792b3c97c129256a6
Stormxeaglex <liamthoreson@gmail.com>
2026-02-23 19:57:40 -0600

    Messed up command

--- Src/VictoryMode.c
+++ Src/VictoryMode.c
@@ -1,92 +1,114 @@
-  
-void PrintVictoryMessages(void) {
-    
-    enum messageStates {
-        INITIALISE = 0,
-        
-        THANK_MARIO = 0,
-        THANK_LUIGI = 1,
-        
-        W1–7_ANOTHER_CASTLE = 2,
-        W1–7_END_DELAY = 3,
-        W1–7_EXIT_MSG = 4,
-        
-        W8_PLAY_PRINCESS_MUSIC = 3,
-        W8_PRINCESS_MSG_1 = 3,
-        W8_PRINCESS_MSG_2 = 4,
-        W8_SECOND_QUEST_MSG_1 = 5,
-        W8_SECOND_QUEST_MSG_2 = 6,
-        W8_EXIT_MSG = 7,
-        
-        UNUSED_STATE9 = 9
+#include "VictoryMode.h"
+
+extern uint8_t rCarry;
+extern void ScrollScreen(register uint8_t) {}
+extern void UpdScrollVar(void) {}
+
+enum VictoryModeSubroutinesOptions {
+    BridgeCollapse,
+    SetupVictoryMode,
+    PlayerVictoryWalk,
+    PrintVictoryMessages,
+    PlayerEndWorld          
+}
+
+void VictoryModeSubroutines(void) {
+    switch (OperMode_Task) {
+        case BridgeCollapse:        goto do_BridgeCollapse;
+        case SetupVictoryMode:      goto do_SetupVictoryMode;
+        case PlayerVictoryWalk:     goto do_PlayerVictoryWalk;
+        case PrintVictoryMessages:  goto do_PrintVictoryMessages;
+        case PlayerEndWorld:        goto do_PlayerEndWorld;
     }
     
-    //
-    // --Handle Logic For Waiting For The Next Valid State--
-    //
-    
-    if (gTimerUntilNextMsgState > 0 || gSavedMsgState >= UNUSED_STATE9) goto WaitUntilNextState;
-
-    // PrimaryMsgCounter != 0 case (else part of the original beq)
-    if (gSavedMsgState > INITIALIZE) {
-        if (WorldNumber == World8) {
-            if (gSavedMsgState < PRINCESS_MSG_1) 
-                goto WaitUntilNextState;
-        } else {
-            // MRetainerMsg (world 1‑7)
-            if (gSavedMsgState < ANOTHER_CASTLE) 
-                goto WaitUntilNextState;
-            //Note the SBC instruction is useless, and its removed from the C
-            //the later iny and dey instructions also always cancel out
+    do_SetupVictoryMode:
+        DestinationPageLoc = ScreenRight_PageLoc++;
+        EventMusicQueue = MUSIC_END_OF_CASTLE
+        OperMode_Task++
+        return;
+        
+    do_PlayerVictoryWalk:
+        //--Check when we should stop player autowalk--
+        gVictoryWalkControl = 
+            ((Player_PageLoc != DestinationPageLoc) || (Player_X_Position < 60));
+        AutoControlPlayer(gVictoryWalkControl);
+        
+        //--Handle Auto Scrolling--
+        if (ScreenLeft_PageLoc != DestinationPageLoc) {
+            {
+                gScrollFractional += 0x80; bool rCarry = (gScrollFraction < 0x80);
+                ScrollScreen(1 + rCarry);
+            }
+            UpdScrollVar();
+            gVictoryWalkControl += 1; //Asm weirdness to keep this true
         }
-        // fall through to ThankPlayer
-    }
+        
+        //--Check if we should enter the next victory mode state after autowalk completes--
+        if (gVictoryWalkControl == 0) {
+            OperMode_Task++;
+        }
+        return;
 
-    //ThankPlayer
-    // --Handle Execute Message State Logic--
-    
-    register u8 WorkingMsgState = gSavedMsgState
-    if (WorkingMsgState == INITIALIZE) {     // Thank you mario or Thank you luigi message
-        WorkingMsgState = (gCurrentPlayer==LUIGI) ? THANK_LUIGI : THANK_MARIO // Luigi uses message 1 
-    } else {                          // After thanking, select world 8 or princess in another castle messages
-        if (WorldNumber == World8) {
-            // EvalForMusic
-            if (WorkingMsgState == W8_PLAY_PRINCESS_MUSIC) {
-                EventMusicQueue = VictoryMusic;
+    do_PrintVictoryMessages:
+        // --Handle Logic For Waiting For The Next Valid State--
+        if (gTimerUntilNextMsgState > 0 || gSavedMsgState >= UNUSED_STATE9) goto WaitUntilNextState;
+        // PrimaryMsgCounter != 0 case (else part of the original beq)
+        if (gSavedMsgState > INITIALIZE) {
+            if (WorldNumber == World8) {
+                if (gSavedMsgState < PRINCESS_MSG_1) 
+                    goto WaitUntilNextState;
+            } else {
+                // MRetainerMsg (world 1‑7)
+                if (gSavedMsgState < ANOTHER_CASTLE) 
+                    goto WaitUntilNextState;
+                //Note the SBC instruction is useless, and its removed from the C
+                //the later iny and dey instructions also always cancel out
             }
-        } else {
-            if (WorkintMsgState == W1–7_END_DELAY) {
-                goto WaitUntilNextState;
-            } else if  (WorkingMsgState >= W1–7_EXIT_MSG) {
+            // fall through to ThankPlayer
+        }
+        //ThankPlayer
+        // --Handle Execute Message State Logic--
+        register MsgStates WorkingMsgState = gSavedMsgState
+        if (WorkingMsgState == INITIALIZE) {     // Thank you mario or Thank you luigi message
+            WorkingMsgState = (gCurrentPlayer==LUIGI) ? THANK_LUIGI : THANK_MARIO // Luigi uses message 1 
+        } else {                          // After thanking, select world 8 or princess in another castle messages
+            if (WorldNumber == World8) {
+                // EvalForMusic
+                if (WorkingMsgState == W8_PLAY_PRINCESS_MUSIC) {
+                    EventMusicQueue = VictoryMusic;
+                }
+            } else {
+                if (WorkintMsgState == W1–7_END_DELAY) {
+                    goto WaitUntilNextState;
+                } else if  (WorkingMsgState >= W1–7_EXIT_MSG) {
+                    goto SetEndTimerAndStopPrinting;
+                }
+            }  
+        }
+        // --Update the Pointer to the message buffer--
+        // PrintMsg
+        //($0c-$0d = first), ($0e = world 1-7's), ($0f-$12 = world 8's)
+        VRAM_Buffer_AddrCtrl = WorkingMsgState + 0x0C;
+        goto WaitUntilNextState;
+        
+        WaitUntilNextState: { // Wait for Secondary timer before displaying next message
+            {
+                gTimerUntilNextMsgState += 4; bool rCarry = (gTimerUntilNextMsgState < 4);
+                gMsgState += rCarry;
+            }
+            if (gMsgState >= W8_EXIT_MSG)
                 goto SetEndTimerAndStopPrinting;
-            }
-        }  
-    }
+            return;
+        }
+        
+        SetEndTimerAndStopPrinting: {
+            WorldEndTimer = 6;
+            OperMode_Task++;
+            return;
+            //Vars are cleared later by code that Ram up to $074b
+        }
+        
+    doPlayerEndWorld:
     
-    // --Update the Pointer to the message buffer--
     
-    // PrintMsg
-
-    //($0c-$0d = first), ($0e = world 1-7's), ($0f-$12 = world 8's)
-    VRAM_Buffer_AddrCtrl = WorkingMsgState + 0x0C;
-    goto WaitUntilNextState;
-
-
-    WaitUntilNextState: { // Wait for Secondary timer before displaying next message
-        // delay timer before displaying next message
-        uint8_t prevValForOverflowCheck = gTimerUntilNextMsgState;
-        gTimerUntilNextMsgState += 4;
-        if (gTimerUntilNextMsgState < prevValForOverflowCheck)
-            gMsgState++;
-        if (gMsgState >= W8_EXIT_MSG)
-            goto SetEndTimerAndStopPrinting;
-        return;
-    }
-    
-    SetEndTimerAndStopPrinting: {
-        WorldEndTimer = 6;
-        OperMode_Task++;
-        return;
-        //Vars are cleared later by code that Ram up to $074b
-    }
 }
