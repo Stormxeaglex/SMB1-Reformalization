995a9344de06ec2781776b46be2d23abc501862f
Stormxeaglex <54553915+Stormxeaglex@users.noreply.github.com>
2026-02-23 19:55:23 -0600

    Delete PrintVictoryMessages.c

--- PrintVictoryMessages.c
+++
@@ -1,8 +0,0 @@
-void PrintVictoryMessages() { //Probably should change this to a label
-  if (SecondaryMsgCounter == 0) {
-    if (PrimaryMsgCounter != 0) { 
-    }
-    { //Thank Player?
-      
-    }
-  }
